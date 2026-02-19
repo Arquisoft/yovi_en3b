5a30783a74b663bd0076b4d057e5bba2343ba462
UO302165 <uo302165@uniovi.es>
2026-02-19 19:59:25 +0100

    users test fixed

--- users/__tests__/index.test.js
+++ users/__tests__/index.test.js
@@ -1,23 +1,34 @@
 import { describe, it, expect, afterEach, vi } from 'vitest'
 import request from 'supertest'
+import { createRequire } from 'node:module';
+
+const require = createRequire(import.meta.url);
 import app from '../index.js'
+
+const db = require('../src/db/db.js');
+
 describe('POST /users/createuser', () => {
     afterEach(() => {
         vi.restoreAllMocks()
     })
 
     it('returns a greeting message for the provided username', async () => {
-        const dynamicName = 'Pablo' + Date.now() 
+        const testName = 'Pablo'
+        vi.spyOn(db, 'query').mockResolvedValue({
+            rows: [{ id: 1, username: testName, email: 'pablo@test.com' }]
+        });
         const res = await request(app)
             .post('/users/createuser')
-            .send({ username: dynamicName})
+            .send({ 
+                username: testName,
+                email: 'pablo@test.com', 
+                password: 'password123'
+            })
             .set('Accept', 'application/json')
 
-        // console.log(res.body)
-        // console.log(res.text)
-        // expect(res.status).toBe(200)
-        // expect(res.body).toHaveProperty('message')
-        // expect(res.body.message).toMatch(/Welcome Pablo/i)
-        expect(true).toBe(true);
+        expect(res.status).toBe(200)
+        expect(res.body).toHaveProperty('message')
+        expect(res.body.message).toMatch(/Welcome Pablo/i)
+       
     })
 })
