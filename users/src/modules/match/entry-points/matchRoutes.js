d99f6ba11c74f4ae5853ee855fea50440fe02aac
UO302165 <uo302165@uniovi.es>
2026-03-24 20:11:25 +0100

    wrong imports solved

--- users/src/modules/match/entry-points/matchRoutes.js
+++ users/src/modules/match/entry-points/matchRoutes.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const matchController = require('./matchController');
 
-router.post('/creatematch', matchController.createMatch);
+router.post('/create', matchController.createMatch);
 
 module.exports = router;
