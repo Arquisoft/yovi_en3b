8f246be1fc73c54e1e776b6df1df969c04fe63c0
UO302165 <uo302165@uniovi.es>
2026-04-01 11:27:29 +0200

    error in the tests solved, the backend port for the tests is the 3001

--- webapp/src/components/SignUp/SignUpForm.tsx
+++ webapp/src/components/SignUp/SignUpForm.tsx
@@ -49,7 +49,7 @@ const SignUpForm: React.FC = () => {
           username: formData.username,
           email: formData.email,
           password: formData.password,
-          avatarId: formData.avatarId
+          photo: formData.avatarId
         }),
       });
 
