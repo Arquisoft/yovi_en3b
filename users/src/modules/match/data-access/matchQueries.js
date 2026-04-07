3dc61c44c93638907efadd796bafdad9c27a6007
UO300022 <UO300022@uniovi.es>
2026-04-07 14:49:11 +0200

    Find match by player_id method

--- users/src/modules/match/data-access/matchQueries.js
+++ users/src/modules/match/data-access/matchQueries.js
@@ -4,5 +4,10 @@ module.exports = {
     INSERT INTO matches (blue_player_id, red_player_id, is_bot, bot_difficulty, status)
     VALUES ($1, $2, $3, $4, 'in_progress')
     RETURNING *; 
-   `
+   `,
+   selectWherePlayerId: `
+    SELECT * FROM matches 
+    WHERE blue_player_id = $1 OR red_player_id = $1
+    ORDER BY created_at DESC;
+    `
 };
