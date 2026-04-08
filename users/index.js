80f0ee601f260f9bfc61e1b6bea871b86db6d5bc
UO302165 <uo302165@uniovi.es>
2026-04-09 00:55:25 +0200

    cors checked

--- users/index.js
+++ users/index.js
@@ -22,9 +22,10 @@ try {
 }
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost');
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
   if (req.method === 'OPTIONS') return res.sendStatus(204);
   next();
 });
